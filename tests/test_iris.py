65d683fee5f19019aaa1d46224a2f1609b590da5
blindcharzard <65547089+blindcharzard@users.noreply.github.com>
2023-11-04 13:07:45 +0100

    Create test_iris.py

---
+++ tests/test_iris.py
@@ -0,0 +1,33 @@
+# Import necessary libraries
+import numpy as np
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+
+# Load the Iris dataset
+iris = load_iris()
+X = iris.data
+y = iris.target
+
+# Split the dataset into train and test sets (80% training, 20% testing)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Create a Decision Tree Classifier and fit it to the training data
+clf = DecisionTreeClassifier(random_state=42)
+clf.fit(X_train, y_train)
+
+# Make predictions on the test set
+y_pred = clf.predict(X_test)
+
+# Evaluate the model's performance
+accuracy = accuracy_score(y_test, y_pred)
+print(f"Accuracy: {accuracy:.2f}")
+
+classification_rep = classification_report(y_test, y_pred, target_names=iris.target_names)
+print("Classification Report:")
+print(classification_rep)
+
+confusion_mat = confusion_matrix(y_test, y_pred)
+print("Confusion Matrix:")
+print(confusion_mat)
