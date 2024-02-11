b4191953ca535d3054f8877539741865b8ef434a
blindcharzard <65547089+blindcharzard@users.noreply.github.com>
2024-02-11 20:55:48 +0100

    Update dftb_test.py

--- tests/dftb_test.py
+++ tests/dftb_test.py
@@ -3,7 +3,7 @@ from torch_dftd.torch_dftd3_calculator import TorchDFTD3Calculator
 
 atoms = molecule("CH3CH2OCH3")
 # device="cuda:0" for fast GPU computation. or "cpu"
-calc = TorchDFTD3Calculator(atoms=atoms, device="cuda:0", damping="bj")
+calc = TorchDFTD3Calculator(atoms=atoms, device=""cpu"", damping="bj")
 
 energy = atoms.get_potential_energy()
 forces = atoms.get_forces()
